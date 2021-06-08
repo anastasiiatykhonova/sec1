7f8c24aebbf470d20da4ea5ebad13426558e3526
Tikhonova Nastya <tikhonovanastya@gmail.com>
2021-06-09 00:11:33 +0300

    1.0.1

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Calc = require('./calc');
 console.log("Тихонова Анастасія");
 console.log("Демонстрація Ряду Тейлора ch(x):");
 
-var steps = 4;
-
+var steps = process.argv[2] ?? 4;
+console.log("Кількість ітерацій: " + steps);
 var res = Calc.TaylorCh(2, steps);
 console.log(res);
