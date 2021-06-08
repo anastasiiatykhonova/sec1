b59fbcf4a3bf61f4ea5886913f65aacf11e9bc2d
Tikhonova Nastya <tikhonovanastya@gmail.com>
2021-06-09 01:17:43 +0300

    1.1.0

--- index.test.js
+++ index.test.js
@@ -6,7 +6,7 @@ describe('Unit test', function () {
     it('Функція ATaylorCh існує', function(){
         assert.equal(typeof Calc.ATaylorCh, "function");
     });
-    it('Has to be equal to 0.5231770833 if members are 3', function () {
+    it('Has to be equal to 0.5231770833 if steps number are 4', function () {
         assert.equal(Calc.ATaylorCh(2, 4), 3.7619);
     })
 });
